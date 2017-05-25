6e8d6e62a67ccde456a9822b24b9f396c2de734f
EddHding <eharding448@stowe.co.uk>
2017-05-25 13:54:28 +0100

    Transaction Seed Data update

--- Accounting.DataBase/AccountingDbInitializer.cs
+++ Accounting.DataBase/AccountingDbInitializer.cs
@@ -6,30 +6,32 @@ using System;
 
 namespace Accounting.DataBase
 {
-    public class AccountingDbInitializer : DropCreateDatabaseIfModelChanges<AccountingDbContext>
+    public class AccountingDbInitializer : DropCreateDatabaseAlways<AccountingDbContext>
     {
         private AccountingDbContext Context;
         protected override void Seed(AccountingDbContext context)
         {
             this.Context = context;
-            AddNewAccount("Stock of goods");
-            AddNewAccount("Building");
-            AddNewAccount("Cash at Bank");
+            var sog = AddNewAccount("Stock of goods");
+            var bld = AddNewAccount("Building");
+            var cab = AddNewAccount("Cash at Bank");
             Context.SaveChanges();
-            AddNewTransaction("Sale to customer", 1, 3, new DateTime(2017, 5, 25), 108.45m);
+            AddNewTransaction("Sale to customer", sog.Id, cab.Id, new DateTime(2017, 5, 25), 108.45m);
 
         }
 
-        private void AddNewAccount(string name)
+        private Account AddNewAccount(string name)
         {
             var ac = new Account() { AccountName = name };
             Context.Accounts.Add(ac);
+            return ac;
         }
 
-        private void AddNewTransaction(string name, int debitAccountId, int creditAccountId, DateTime date, decimal amount)
+        private Transaction AddNewTransaction(string name, int debitAccountId, int creditAccountId, DateTime date, decimal amount)
         {
             var tr = new Transaction() { Name = name, DebitAccountId = debitAccountId, CreditAccountId = creditAccountId, Date = date, Amount = amount};
             Context.Transactions.Add(tr);
+            return tr;
         }
     }
 }
