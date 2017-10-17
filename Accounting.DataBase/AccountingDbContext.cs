11662ba3173be911d30f85f2f78d4e0282eecb82
EddHding <eharding448@stowe.co.uk>
2017-10-17 09:40:54 +0100

    First go at manually mapping database tables

--- Accounting.DataBase/AccountingDbContext.cs
+++ Accounting.DataBase/AccountingDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
 using Accounting.Model;
 
 namespace Accounting.DataBase
@@ -13,6 +15,42 @@ namespace Accounting.DataBase
         public DbSet<BalanceSheet> BalanceSheets { get; set; }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<AbstractTransaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            DefineAccount(modelBuilder.Entity<Account>());
+            DefineAbstractTransaction(modelBuilder.Entity<AbstractTransaction>());
+            //DefineBalance(modelBuilder.Entity<Balance>());
+            //DefineBalanceSheet(modelBuilder.Entity<BalanceSheet>());
+            //DefineAbstractTransaction(modelBuilder.Entity<AbstractTransaction>());
+
+        }
+
+        private void DefineBalanceSheet(EntityTypeConfiguration<BalanceSheet> bs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DefineBalance(EntityTypeConfiguration<Balance> b)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DefineAbstractTransaction(EntityTypeConfiguration<AbstractTransaction> atConfig)
+        {
+            atConfig.ToTable("Transaction Record");
+            atConfig.HasKey(at => at.Id);
+            atConfig.Property(at => at.Date).HasColumnName("DATE OF TRANSACTION");
+            atConfig.Property(at => at.Amount).HasColumnName("AMOUNT");
+        }
+
+        private void DefineAccount(EntityTypeConfiguration<Account> accountConfig)
+        {
+            accountConfig.ToTable("Account Record");
+            accountConfig.HasKey(a => a.Id);
+            accountConfig.Property(a => a.AccountName).HasColumnName("ACCOUNT NAME");
+            accountConfig.Property(a => a.TypeOfAccount).HasColumnName("TYPE OF ACCOUNT");
+        }
     }
 
 }
